4e76c15b56f165d357a16dcc55897eb1a634e10f
jimao <shihao.gu@perfma.com>
2024-11-24 21:40:37 +0800

    Button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -5,5 +5,15 @@ interface IButtonProps {
 }
 
 export default function Button({ children }: IButtonProps) {
-  return <button className="bg-blue-500 text-white rounded-sm px-2 py-1">{children}</button>;
+  return <button className="
+    bg-blue-500
+    text-white
+    rounded-sm
+    px-2
+    py-1
+    hover:bg-blue-600
+    transition-colors
+    duration-300
+    ease-in-out
+  ">{children}</button>;
 }
