25ee58d2ceb3968843768b58bcbc938ecf597148
jimao <shihao.gu@perfma.com>
2024-11-25 15:25:14 +0800

    提交

--- src/app/phrase/[id]/page.tsx
+++ src/app/phrase/[id]/page.tsx
@@ -1,6 +1,13 @@
 
 export default function Phrase() {
-  return <div>
-    <span>你好，世界</span>
-  </div>;
+  return (
+    <div className="h-[100dvh] flex flex-col">
+      <div className="h-1/2">
+        <textarea className="w-full h-full resize-none outline-none p-4 bg-transparent" />
+      </div>
+      <div className="h-1/2 border-t">
+        <textarea className="w-full h-full resize-none outline-none p-4 bg-transparent" />
+      </div>
+    </div>
+  );
 }
