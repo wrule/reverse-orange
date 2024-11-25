efbdb311ea744baab5390b3a383382d14590fa05
jimao <shihao.gu@perfma.com>
2024-11-25 12:16:33 +0800

    提交

--- src/components/Orange.tsx
+++ src/components/Orange.tsx
@@ -15,7 +15,7 @@ export default function Orange(props: {
   return (
     <div className="w-full min-h-8 px-2 py-1 border-t">
       <div className="flex">
-        <span className="text-lg font-bold">13</span>
+        <span className="text-lg font-bold leading-5">13</span>
       </div>
       <p className="font-bold indent-[2em]">Orange is my cat, is not a fruit, it's my cat's name. Haha that's nice</p>
       <p className="italic text-sm indent-[2.3em]">欧润橘是我的猫，它不是一个水果，它是我的猫的名字，哈哈，这很有趣</p>
