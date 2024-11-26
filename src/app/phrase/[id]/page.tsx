7380b4c7435ecacf2bb45cfc9abb251b94886eaa
jimao <shihao.gu@perfma.com>
2024-11-26 12:03:56 +0800

    提交

--- src/app/phrase/[id]/page.tsx
+++ src/app/phrase/[id]/page.tsx
@@ -8,7 +8,7 @@ export default function Phrase() {
   const { id } = useParams();
   const [phrase, setPhrase] = useState<string>('');
   const [translation, setTranslation] = useState<string>('');
-  const [orange, setOrange] = useState<IOrange>();
+  const [orange, setOrange] = useState<IOrange>({ } as IOrange);
 
   const fetchOrange = async () => {
     const orange = await getOrange(Number(id));
@@ -26,12 +26,22 @@ export default function Phrase() {
   return (
     <div className="h-[100dvh] flex flex-col">
       <div className="flex-1">
-        <textarea value={phrase} onChange={(event) => setPhrase(event.target.value)} className="w-full h-full resize-none outline-none p-4 bg-transparent" />
+        <textarea
+          placeholder="Please input english phrase here..."
+          value={phrase}
+          onChange={(event) => setPhrase(event.target.value)}
+          className="text-xl w-full h-full resize-none outline-none p-4 bg-transparent"
+        />
       </div>
       <div className="flex-1 border-t">
-        <textarea value={translation} onChange={(event) => setTranslation(event.target.value)} className="w-full h-full resize-none outline-none p-4 bg-transparent" />
+        <textarea
+          placeholder="Please input translation here..."
+          value={translation}
+          onChange={(event) => setTranslation(event.target.value)}
+          className="text-xl w-full h-full resize-none outline-none p-4 bg-transparent"
+        />
       </div>
-      <div className="h-14 bg-blue-700 text-white text-2xl flex items-center justify-center cursor-pointer">Save</div>
+      <div className="h-[3.3rem] bg-blue-700 text-white text-xl flex items-center justify-center cursor-pointer">Save</div>
     </div>
   );
 }
