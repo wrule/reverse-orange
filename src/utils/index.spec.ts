7b9c455115e9c60727a759948cbd28a0566be118
jimao <shihao.gu@perfma.com>
2024-11-26 13:57:59 +0800

    单文件

--- src/utils/index.spec.ts
+++ src/utils/index.spec.ts
@@ -1,7 +1,14 @@
 import { add, sub } from '.';
 
 describe('describe', () => {
-  it('it', () => {
-    expect(1).toEqual(1);
+  it('add', () => {
+    const num1 = Math.random() * 10000;
+    const num2 = Math.random() * 10000;
+    expect(add(num1, num2)).toEqual(num1 + num2);
+  });
+  it('sub', () => {
+    const num1 = Math.random() * 10000;
+    const num2 = Math.random() * 10000;
+    expect(sub(num1, num2)).toEqual(num1 - num2);
   });
 });
