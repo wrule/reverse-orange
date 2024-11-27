90430f484c18e420cd7c982ac9d302765e8b5823
jimao <shihao.gu@perfma.com>
2024-11-27 13:47:11 +0800

    调整loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,7 +2,7 @@
 const Loading = () => {
   return (
     <div className="w-full h-[40rem] flex items-center justify-center">
-      <div className="w-20 h-20 border-4 border-gray-200 border-t-blue-700 rounded-full animate-spin"></div>
+      <div className="w-14 h-14 border-4 border-gray-200 border-t-blue-700 rounded-full animate-spin"></div>
     </div>
   );
 }
