89ced5737fca951395ff006d5171b431f374995e
jimao <shihao.gu@perfma.com>
2024-11-27 11:43:33 +0800

    优化组件

--- src/components/Orange.tsx
+++ src/components/Orange.tsx
@@ -5,19 +5,17 @@ import { useRef } from 'react';
 import { useRouter } from 'next/navigation';
 import { IOrange } from '@/common/db';
 
-export default function Orange(props: {
-  orange: IOrange,
-}) {
+export default function Orange({ orange }: { orange: IOrange }) {
   const router = useRouter();
   const [phrase, speak] = useSpeaker();
   const timer = useRef<number | null>(null);
 
   const handleClick = () => {
-    speak(props.orange.phrase);
+    speak(orange.phrase);
   };
 
   const handleDoubleClick = () => {
-    router.push(`/phrase/${props.orange.id}`);
+    router.push(`/phrase/${orange.id}`);
   };
 
   const handleUserClick = () => {
@@ -35,17 +33,17 @@ export default function Orange(props: {
 
   return (
     <div
-      className={`w-full min-h-8 px-2 py-2 border-t transition-colors duration-300 ${phrase === props.orange.phrase ? 'bg-blue-500' : ''}`}
+      className={`w-full min-h-8 px-2 py-2 border-t transition-colors duration-300 ${phrase === orange.phrase ? 'bg-blue-500' : ''}`}
       onClick={() => handleUserClick()}>
       <div className="flex justify-between items-center ">
-        <span className="text-2xl font-bold leading-7">{props.orange.id}.</span>
+        <span className="text-2xl font-bold leading-7">{orange.id}.</span>
         <span>
-          <span className="bg-green-400 px-2 py-[0.1rem] rounded-tl-sm rounded-bl-sm">{props.orange.correctCount}</span>
-          <span className="bg-red-400 px-2 py-[0.1rem] rounded-tr-sm rounded-br-sm">{props.orange.incorrectCount}</span>
+          <span className="bg-green-400 px-2 py-[0.1rem] rounded-tl-sm rounded-bl-sm">{orange.correctCount}</span>
+          <span className="bg-red-400 px-2 py-[0.1rem] rounded-tr-sm rounded-br-sm">{orange.incorrectCount}</span>
         </span>
       </div>
-      <p className="text-xl font-bold indent-[2em]">{props.orange.phrase}</p>
-      <p className="italic text-base indent-[2.5em] mt-2">{props.orange.translation}</p>
+      <p className="text-xl font-bold indent-[2em]">{orange.phrase}</p>
+      <p className="italic text-base indent-[2.5em] mt-2">{orange.translation}</p>
     </div>
   );
 }
