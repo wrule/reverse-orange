4fdc9cd118b469498667de4a969eefbcf3d210a3
jimao <shihao.gu@perfma.com>
2024-11-27 16:19:35 +0800

    提交

--- src/app/train/page.tsx
+++ src/app/train/page.tsx
@@ -5,7 +5,7 @@ import Question from '@/components/Question';
 export default function Train() {
   return (
     <div>
-      <Question orange={{ translation: '这是一个测试，所以请你先别在意。' } as any} />
+      <Question orange={{ translation: '这是一个测试，所以请你先别在意。', phrase: 'This is a test, please dont care about it' } as any} />
     </div>
   );
 }
--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,18 +1,32 @@
 'use client';
 
 import { IOrange } from '@/common/db';
+import { useEffect, useState } from 'react';
 
 export default function Question({ orange }: { orange: IOrange }) {
+  const [isFull, setIsFull] = useState<boolean>(true);
+
+  useEffect(() => {
+    setIsFull(false);
+  }, []);
+
   return (
     <div>
-      <div className="mx-2 bg-gray-400 h-2 rounded-sm mb-10 mt-32">
-        <div className="bg-blue-700 w-[50%] h-full rounded-sm"></div>
+      <div className="mx-4 bg-gray-300 h-2 rounded-sm mb-10 mt-24">
+        <div
+          className="bg-blue-700 h-full rounded-sm transition-[width]"
+          style={{
+            width: isFull ? '100%' : '0%',
+            transition: 'width 60s',
+          }}>
+        </div>
       </div>
       <p className="mx-2 text-center font-bold text-[1.6rem] mb-8">{orange.translation}</p>
-      <div className="flex justify-center gap-4">
+      <div className="flex justify-center gap-4 mb-8">
         <button className="inline-block w-32 text-white bg-blue-700 px-3 py-2 rounded-sm">I Can Speak</button>
         <button className="inline-block w-32 text-white bg-red-700 px-3 py-2 rounded-sm">I Can't Speak</button>
       </div>
+      <p className="mx-3 text-gray-400 text-center font-bold text-[1.6rem] mb-8">{orange.phrase}</p>
     </div>
   );
 }
