6944aa3bffda4aa412c600c9684ad049c912a9bd
jimao <shihao.gu@perfma.com>
2024-11-27 14:04:08 +0800

    优化

--- src/app/phrase/[id]/page.tsx
+++ src/app/phrase/[id]/page.tsx
@@ -25,6 +25,7 @@ export default function Phrase() {
   }, [id]);
 
   const handleSave = async () => {
+    if (!orange.phrase.trim() || !orange.translation.trim()) return;
     try {
       if (id === 'add') {
         await addOrange(orange);
